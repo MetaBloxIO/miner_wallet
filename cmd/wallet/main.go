21163f8530eb39036ece7e22ddae4fefb0bc11c8
Liwanghong <wanghong@dappworks.cn>
2022-04-12 14:29:24 +0800

    add go harmony test code

--- cmd/wallet/main.go
+++ cmd/wallet/main.go
@@ -1,23 +1,48 @@
-package main 
+package main
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
 	"log"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
-	conn, err := ethclient.Dial("https://mainnet.infura.io")
+	conn, err := ethclient.Dial("https://api.s0.b.hmny.io")
 	if err != nil {
 		log.Fatal("Whoops something went wrong!", err)
+		return
 	}
 
 	ctx := context.Background()
-	tx, pending, _ := conn.TransactionByHash(ctx, common.HexToHash("0x30999361906753dbf60f39b32d3c8fadeb07d2c0f1188a32ba1849daac0385a8"))
-	if !pending {
-		fmt.Println(tx)
+	privateKey, err := crypto.HexToECDSA("fa4036001975229cdd22d3b02302b7282e21dcae04a0c1efe1844de8c8576b4f")
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println("SAVE BUT DO NOT SHARE THIS (Private Key):", hexutil.Encode(privateKeyBytes))
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	fmt.Println("Public Key:", hexutil.Encode(publicKeyBytes))
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	balance, err := conn.BalanceAt(ctx, address, nil)
+	if err != nil {
+		log.Fatal("Get balance error", err)
+		return
+	}
+
+	log.Print("balance value ", balance)
 }
