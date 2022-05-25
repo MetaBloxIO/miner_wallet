e822ce13944b524b8b941d2426051b707105ff4f
Liwanghong <wanghong@dappworks.cn>
2022-05-25 19:20:25 +0800

    remove duplicated read conf

--- cmd/wallet/main.go
+++ cmd/wallet/main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	conf2 "github.com/MetaBloxIO/miner_wallet/conf"
 	"github.com/MetaBloxIO/miner_wallet/server"
-	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	_, err := conf2.LoadConf("./conf/config.json")
-
-	if err != nil {
-		log.Errorf("Read config file failed")
-		return
-	}
-
 	//TODO create eth client using conf
 
 	router := server.InitRouter()
