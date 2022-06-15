cd2a84e6fa15b4bb1e8b3415d23ecafa26f5dbfe
Liwanghong <wanghong@dappworks.cn>
2022-06-15 10:59:03 +0800

    move to harmony devnet

--- cmd/wallet/main.go
+++ cmd/wallet/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/MetaBloxIO/metablox-foundation-services/contract"
 	"github.com/MetaBloxIO/miner_wallet/server"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -10,6 +11,8 @@ func main() {
 
 	router := server.InitRouter()
 
+	viper.SetConfigFile("./config.yaml")
+	viper.ReadInConfig()
 	contract.Init()
 
 	router.Run(":3000")
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/MetaBloxIO/miner_wallet
 go 1.18
 
 require (
-	github.com/MetaBloxIO/metablox-foundation-services v0.0.0-20220531132944-8b5e5dec37f5
+	github.com/MetaBloxIO/metablox-foundation-services v0.0.0-20220613081924-6f30f78100b6
 	github.com/ethereum/go-ethereum v1.10.17
 	github.com/gin-gonic/gin v1.7.7
 	github.com/google/uuid v1.2.0
